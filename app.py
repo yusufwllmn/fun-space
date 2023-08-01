bd0e1d9f7bde94984823129566a64b13bab34751
yusufwllmn <yoesoefwillman06@gmail.com>
2023-08-01 10:18:06 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,8 @@
+# Hitung Luas Segitiga sederhana
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
